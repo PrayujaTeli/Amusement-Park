e8605d284bcbff4d9735f0202cd18439e1899dda
Prayuja Teli <prayujateli@gmail.com>
2024-04-10 12:46:39 -0400

    Create WDAmusmentPark.sql

---
+++ SQL_Queries/WDAmusmentPark.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 
+    WDAmusmentPark 
+    ( 
+        ParkID        INTEGER, 
+        ParkName      TEXT, 
+        OpenTime      TIME, 
+        ClosingTime   TIME, 
+        Country       TEXT, 
+        State         TEXT, 
+        City          TEXT, 
+        StreetAddress TEXT, 
+        PRIMARY KEY (ParkID) 
+    );
