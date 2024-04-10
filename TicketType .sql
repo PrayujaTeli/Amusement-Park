ecf7455e7ef85940a471473320e8de4cf6fb8579
Prayuja Teli <prayujateli@gmail.com>
2024-04-10 12:56:40 -0400

    Create TicketType .sql

---
+++ TicketType .sql
@@ -0,0 +1,9 @@
+CREATE TABLE 
+    TicketType 
+    ( 
+        TicketTypeID       INTEGER, 
+        TicketName         TEXT, 
+        TicketRestrictions TEXT, 
+        TicketPrice        NUMERIC, 
+        PRIMARY KEY (TicketTypeID) 
+    );
