ce6a116cd2449a1a8710c8814c678704ef702d27
Prayuja Teli <prayujateli@gmail.com>
2024-04-10 12:57:20 -0400

    Delete TicketType .sql

--- TicketType .sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE 
-    TicketType 
-    ( 
-        TicketTypeID       INTEGER, 
-        TicketName         TEXT, 
-        TicketRestrictions TEXT, 
-        TicketPrice        NUMERIC, 
-        PRIMARY KEY (TicketTypeID) 
-    );
